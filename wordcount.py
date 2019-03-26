171bb2b03a4f3a50851f5d24f7c9554bb58fd9f1
Pablo Mandiola <pmb434@nyu.edu>
2019-03-26 19:25:08 -0400

    init

---
+++ wordcount.py
@@ -0,0 +1,9 @@
+from pyspark import SparkContext()
+
+if __name__ == '__main__'
+    sc = SparkContext()
+    sc.textFile('book.txt') \
+        .flatMap(lambda line: line.split()) \
+        .map(lambda x: (x.lower(),1)) \
+        .reduceByKey(lambda x,y: x+y) \
+        .saveAsTextFile('output')
